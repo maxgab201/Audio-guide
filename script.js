5d14bb59829def022f80b8f6c735933464c9017d
maxgab201 <maxgab201@gmail.com>
2025-11-30 14:39:47 -0300

    Refactor comments and verify coordinates in script.js
    Updated comments and ensured coordinates are accurate.

--- script.js
+++ script.js
@@ -44,11 +44,11 @@ const locations = [
         mp3File: "Museo del Vidrio Capaldo.m4a",
         playlist: null
     },
-    // --- ESTACIÓN PLÁTANOS (CREADA DE NUEVO CON VERSIONES) ---
+    // --- ESTACIÓN PLÁTANOS (CORREGIDA) ---
     {
         id: 7,
         name: "Estación Plátanos",
-        coords: [-34.7800, -58.1850],
+        coords: [-34.7800, -58.1850], // Coordenadas aseguradas
         img: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1d/Estaci%C3%B3n_Pl%C3%A1tanos.jpg/640px-Estaci%C3%B3n_Pl%C3%A1tanos.jpg",
         categoryType: "transport",
         fallbackIcon: "fa-train",
@@ -61,7 +61,7 @@ const locations = [
         // Archivo por defecto
         mp3File: "Plátanos Station-Corral.m4a"
     },
-    // -----------------------------------------------------------
+    // -------------------------------------
     {
         id: 8,
         name: "Fábrica Rigolleau",
@@ -114,6 +114,7 @@ const locations = [
         categoryType: "museum",
         fallbackIcon: "fa-building",
         brief: "Cultural activities center.",
+        // Versiones Centro De Vicenzo
         versions: [
             { name: "Activity Center (Sama)", file: "Centro de actividades R. de Vicenzo Sama.m4a" },
             { name: "Bio De Vicenzo (Maurizi)", file: "Roberto De Vicenzo- Maurizi.m4a" }
@@ -140,6 +141,7 @@ const locations = [
         fallbackIcon: "fa-map-marker-alt",
         brief: "History of the locality.",
         mp3File: null,
+        // AUTOMATIC PLAYLIST (Unified Bar)
         playlist: [
             { src: "el pato 1 Staniscia.m4a", estimatedDuration: 90 },
             { src: "el pato 2 Staniscia.m4a", estimatedDuration: 120 },
@@ -273,7 +275,7 @@ const locations = [
     }
 ];
 
-// --- GLOBAL VARIABLES ---
+// --- VARIABLES GLOBALES ---
 let map;
 let audioPlayer;
 let currentPlaylist = [];
@@ -331,6 +333,7 @@ window.onload = function() {
                 <div class="popup-details">
                     <h3 class="popup-title">${loc.name}</h3>
                     <p class="popup-desc">${loc.brief}</p>
+                    
                     ${selectorHTML}
                 </div>
                 <div class="player-ui">
@@ -422,6 +425,7 @@ window.seekAudio = function(value) {
         } else {
             audioPlayer.currentTime = seekTimeWithinTrack;
         }
+
     } else {
         const seekTime = audioPlayer.duration * (value / 100);
         audioPlayer.currentTime = seekTime;
@@ -508,6 +512,7 @@ function playTrack(src, slider, errorMsg, btn) {
     audioPlayer.ontimeupdate = function() {
         if (audioPlayer.duration) {
             let percent = 0;
+            
             if (isPlaylistMode) {
                 const totalDuration = getPlaylistTotalDuration();
                 const startTime = getCurrentTrackStartTime();
@@ -516,6 +521,7 @@ function playTrack(src, slider, errorMsg, btn) {
             } else {
                 percent = (audioPlayer.currentTime / audioPlayer.duration) * 100;
             }
+            
             if(percent > 100) percent = 100;
             slider.value = percent;
             updateSliderVisual(slider);
